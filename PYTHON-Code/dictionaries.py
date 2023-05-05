10e65806cd4e28bc580cfd7239d680e46ec2ff84
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-05-05 13:53:17 +0530

    :zap: function sclice loops

---
+++ PYTHON-Code/dictionaries.py
@@ -0,0 +1,13 @@
+person = {
+    "key": "value",
+    "name": "Krishna",
+    "twitter": "@krishna3644",
+}
+# print(person['twitter'])
+
+print(person)
+person['instagram'] = "_a_k_akrishna"
+print(person)
+del person['key']
+print(person)
+
