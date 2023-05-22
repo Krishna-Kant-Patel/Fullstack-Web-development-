125543752c911275bb1110f2e10d332445e28bce
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-05-22 19:57:03 +0530

    :rocket: get post data from data base

--- Django/MyApp/views.py
+++ Django/MyApp/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from .models import Post
 
 class HomePage(TemplateView):
     template_name = "home.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Data'] = Post.objects.all()
+        return context
